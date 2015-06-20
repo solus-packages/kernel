ecf35f79e08aee02928ee1c260f8d2a3c7da8ffb
Ikey Doherty <ikey@solus-project.com>
2015-06-20 12:20:11 +0100

    Update to 4.0.5, and add EFI postinstall

--- comar/package.py
+++ comar/package.py
@@ -1,8 +1,16 @@
 #!/usr/bin/env python
 import os
+import shutil
 
-KernelVersion = "4.0.3"
+KernelVersion = "4.0.5"
 
 def postInstall(fromVersion, fromRelease, toVersion, toRelease):
     # Must run depmod to keep the modules up to date :)
     os.system("/sbin/depmod %s" % KernelVersion)
+
+    kernel = "/boot/kernel-%s" % KernelVersion
+    if os.path.exists("/boot/efi/solus"):
+        try:
+            shutil.copy(kernel, "/boot/efi/solus/kernel")
+        except Exception, e:
+            print("Failed to copy efi kernel")
