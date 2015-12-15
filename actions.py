11558e452623ef05e896c7b88c60316b9ce4e3e3
Justin Zobel <justin@solus-project.com>
2015-12-16 08:12:36 +1030

    Update to 4.1.15

--- actions.py
+++ actions.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python
 # -*- coding: utf-8 -*-
 #
-KVERSION = "4.1.14"
+KVERSION = "4.1.15"
 
 from pisi.actionsapi import kerneltools
 from pisi.actionsapi import shelltools
--- comar/package.py
+++ comar/package.py
@@ -2,7 +2,7 @@
 import os
 import shutil
 
-KernelVersion = "4.1.14"
+KernelVersion = "4.1.15"
 
 def postInstall(fromVersion, fromRelease, toVersion, toRelease):
     # Must run depmod to keep the modules up to date :)
