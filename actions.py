788256accb8b7f8cf1dcf03eed451cbe265eb191
Ikey Doherty <ikey@solus-project.com>
2016-08-06 01:06:37 +0100

    Update to 4.7.0, the latest upstream kernel
    For full details on this change, including the hw enabling, see:
    
     - https://dev.solus-project.com/T54
    
    Signed-off-by: Ikey Doherty <ikey@solus-project.com>

--- actions.py
+++ actions.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python
 # -*- coding: utf-8 -*-
 #
-KVERSION = "4.4.16"
+KVERSION = "4.7.0"
 
 from pisi.actionsapi import kerneltools, shelltools, autotools, pisitools, get
 import os
--- comar/package.py
+++ comar/package.py
@@ -2,7 +2,7 @@
 import os
 import shutil
 
-KernelVersion = "4.4.16"
+KernelVersion = "4.7.0"
 
 def postInstall(fromVersion, fromRelease, toVersion, toRelease):
     # Must run depmod to keep the modules up to date :)
