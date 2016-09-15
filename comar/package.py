1676fc6247fce4771738e603470de42c2d216109
Ikey Doherty <ikey@solus-project.com>
2016-09-15 18:13:32 +0100

    Update to the latest upstream kernel, 4.7.4
     - Enabled CONFIG_DRM_UDL for KMS support of USB DisplayLink devices
     - Switched scheduler defaults to ondemand, vs previous performance
    
    Signed-off-by: Ikey Doherty <ikey@solus-project.com>

--- comar/package.py
+++ comar/package.py
@@ -2,7 +2,7 @@
 import os
 import shutil
 
-KernelVersion = "4.7.3"
+KernelVersion = "4.7.4"
 
 def postInstall(fromVersion, fromRelease, toVersion, toRelease):
     # Must run depmod to keep the modules up to date :)
