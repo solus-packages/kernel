a9821c0798651dcd514e8645bc3a5c84d66b74ec
Ikey Doherty <ikey@solus-project.com>
2016-09-24 16:14:40 +0100

    Update to the latest upstream release, 4.7.5
    Signed-off-by: Ikey Doherty <ikey@solus-project.com>

--- actions.py
+++ actions.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python
 # -*- coding: utf-8 -*-
 #
-KVERSION = "4.7.4"
+KVERSION = "4.7.5"
 
 from pisi.actionsapi import kerneltools, shelltools, autotools, pisitools, get
 import os
--- comar/package.py
+++ comar/package.py
@@ -2,7 +2,7 @@
 import os
 import shutil
 
-KernelVersion = "4.7.4"
+KernelVersion = "4.7.5"
 
 def postInstall(fromVersion, fromRelease, toVersion, toRelease):
     # Must run depmod to keep the modules up to date :)
