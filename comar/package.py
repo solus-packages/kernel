60f5558cb01d6e54f0d3eaae02c3552fc6faa356
Joshua Strobl <joshua@stroblindustries.com>
2016-11-01 21:00:19 +0200

    Update to 4.8.6

--- comar/package.py
+++ comar/package.py
@@ -2,7 +2,7 @@
 import os
 import shutil
 
-KernelVersion = "4.8.5"
+KernelVersion = "4.8.6"
 
 def postInstall(fromVersion, fromRelease, toVersion, toRelease):
     # Must run depmod to keep the modules up to date :)
